d50e510785fe7da42f95917836896c5b7e104b32
jaugustyn <kub4a1@gmail.com>
2022-02-13 19:14:57 +0100

    Views of profiles and movies

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import Home
+from .views import Home, ProfileList, ProfileCreate, MovieList, MovieDetails, MovieShow
 
 app_name = 'core'
 
 urlpatterns = [
     path('', Home.as_view()),
+    path('profile', ProfileList.as_view(), name='profile_list'),
+    path('profile/create', ProfileCreate.as_view(), name='profile_create'),
+    path('movies/<str:profile_id>', MovieList.as_view(), name='movie_list'),
+    path('movie/details/<str:movie_id>', MovieDetails.as_view(), name='movie_details'),
+    path('movie/play/<str:movie_id>', MovieShow.as_view(), name='movie_show'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,73 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.shortcuts import render, redirect
 from django.views import View
+from .forms import ProfileForm
+from .models import Profile, Movie
 
 
 # Create your views here.
 
 class Home(View):
     def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('core:profile_list')
         return render(request, 'index.html')
+
+
+@method_decorator(login_required(login_url='/accounts/login/'), name='dispatch')
+class ProfileList(View):
+    def get(self, request):
+        profiles = request.user.profiles.all()
+        return render(request, 'profile_list.html', {'profiles': profiles})
+
+
+@method_decorator(login_required(login_url='/accounts/login/'), name='dispatch')
+class ProfileCreate(View):
+    def get(self, request):
+        form = ProfileForm
+        return render(request, 'profile_create.html', {'form': form})
+
+    def post(self, request):
+        form = ProfileForm(request.POST or None)
+        if form.is_valid():
+            profile = Profile.objects.create(**form.cleaned_data)
+            if profile:
+                request.user.profiles.add(profile)
+                return redirect('core:profile_list.html')
+        return render(request, 'profile_create.html', {'form': form})
+
+
+@method_decorator(login_required(login_url='/accounts/login/'), name='dispatch')
+class MovieList(View):
+    def get(self, request, profile_id, *args, **kwargs):
+        try:
+            profile = Profile.objects.get(uuid=profile_id)
+            movies = Movie.objects.filter(age_limit=profile.age_limit)
+
+            if profile not in request.user.profiles.all():
+                return redirect('core:profile_list')
+            return render(request, 'movie_list.html', {'movies': movies})
+        except Profile.DoesNotExist:
+            return redirect('core:profile_list')
+
+
+@method_decorator(login_required(login_url='/accounts/login/'), name='dispatch')
+class MovieDetails(View):
+    def get(self, request, movie_id):
+        try:
+            movie = Movie.objects.filter(uuid=movie_id)
+            return render(request, 'movie_detail.html', {'movie': movie})
+        except Movie.DoesNotExist:
+            return redirect('core:movie_list')
+
+
+@method_decorator(login_required(login_url='/accounts/login/'), name='dispatch')
+class MovieShow(View):
+    def get(self, request, movie_id):
+        try:
+            movie = Movie.objects.get(uuid=movie_id)
+            movie = movie.videos.value()
+            return render(request, 'movie_show.html', {'movie': list(movie)})
+        except Movie.DoesNotExist:
+            return redirect('core:movie_list')
